7e6e2ddbc7d1c25e1ca4b1e1c75174f1e20a4a10
Mahdi-Soultana <soultana.mahdi24@gmail.com>
2023-02-03 19:27:21 +0100

    first commit

---
+++ src/components/layout/Layout.tsx
@@ -0,0 +1,15 @@
+import { ReactNode } from 'react';
+import Footer from './footer/Footer';
+import Header from './header/Header';
+
+function Layout({ children }: { children: ReactNode }) {
+  return (
+    <section className="bg-gray-50 w-full h-screen">
+      <Header />
+      {children}
+      <Footer />
+    </section>
+  );
+}
+
+export default Layout;
---
+++ src/components/layout/header/Header.tsx
@@ -0,0 +1,10 @@
+function Header() {
+  return (
+    <header>
+      <nav>Nav</nav>
+      Header
+    </header>
+  );
+}
+
+export default Header;
